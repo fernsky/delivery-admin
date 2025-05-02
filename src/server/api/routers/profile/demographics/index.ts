53e9948f08be9b9b05776a8ec5cc415a1c9b2352
fernsky <trilochanbhusal000@gmail.com>
2025-05-02 21:23:55 +0545

    feat: Add ward age-wise population router to demographics API

--- src/server/api/routers/profile/demographics/index.ts
+++ src/server/api/routers/profile/demographics/index.ts
@@ -2,9 +2,11 @@ import { createTRPCRouter } from "@/server/api/trpc";
 import { demographicSummaryRouter } from "./demographic-summary.procedure";
 import { wardTimeSeriesRouter } from "./ward-time-series.procedure";
 import { wardWiseDemographicSummaryRouter } from "./ward-wise-demographic-summary.procedure";
+import { wardAgeWisePopulationRouter } from "./ward-age-wise-population.procedure";
 
 export const demographicsRouter = createTRPCRouter({
   summary: demographicSummaryRouter,
   wardTimeSeries: wardTimeSeriesRouter,
-  wardWiseDemographicSummary: wardWiseDemographicSummaryRouter, // Add other demographic-related routers here in the future
+  wardWiseDemographicSummary: wardWiseDemographicSummaryRouter,
+  wardAgeWisePopulation: wardAgeWisePopulationRouter,
 });
