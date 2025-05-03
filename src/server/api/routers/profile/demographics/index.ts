c716a234ec2c17203a25904d1fb23cdd06dc5648
fernsky <trilochanbhusal000@gmail.com>
2025-05-03 09:30:40 +0545

    feat: Add ward-wise house head gender router to demographics API

--- src/server/api/routers/profile/demographics/index.ts
+++ src/server/api/routers/profile/demographics/index.ts
@@ -4,11 +4,14 @@ import { wardTimeSeriesRouter } from "./ward-time-series.procedure";
 import { wardWiseDemographicSummaryRouter } from "./ward-wise-demographic-summary.procedure";
 import { wardWiseCastePopulationRouter } from "./ward-wise-caste-population.procedure";
 import { wardAgeWisePopulationRouter } from "./ward-age-wise-population.procedure";
+import { wardWiseHouseHeadGenderRouter } from "./ward-wise-househead-gender.procedure";
+import { wardWiseHouseHeadGender } from "@/server/db/schema/profile/demographics/ward-wise-househead-gender";
 
 export const demographicsRouter = createTRPCRouter({
   summary: demographicSummaryRouter,
   wardTimeSeries: wardTimeSeriesRouter,
   wardWiseDemographicSummary: wardWiseDemographicSummaryRouter,
   wardAgeWisePopulation: wardAgeWisePopulationRouter,
-  wardWiseCastePopulation: wardWiseCastePopulationRouter
+  wardWiseCastePopulation: wardWiseCastePopulationRouter,
+  wardWiseHouseHeadGender: wardWiseHouseHeadGenderRouter,
 });
