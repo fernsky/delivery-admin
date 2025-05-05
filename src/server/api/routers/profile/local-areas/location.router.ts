220a62d1e4f335bf8b6895bf6197eb6caf418458
fernsky <trilochanbhusal000@gmail.com>
2025-05-05 17:33:13 +0545

    feat(location): implement local area details, media, and map components; add slug-based location retrieval

--- src/server/api/routers/profile/local-areas/location.router.ts
+++ src/server/api/routers/profile/local-areas/location.router.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { getAllLocations } from "./procedures/get-locations";
 import { getLocationById } from "./procedures/get-location-by-id";
+import { getLocationBySlug } from "./procedures/get-location-by-slug";
 import { createLocation } from "./procedures/create-location";
 import { updateLocation } from "./procedures/update-location";
 import { deleteLocation } from "./procedures/delete-location";
@@ -9,6 +10,7 @@ import { deleteLocation } from "./procedures/delete-location";
 export const locationRouter = createTRPCRouter({
   getAll: getAllLocations,
   getById: getLocationById,
+  getBySlug: getLocationBySlug,
   create: createLocation,
   update: updateLocation,
   delete: deleteLocation,
