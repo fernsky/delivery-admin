43f4ebf523d6ba7e480d0d43c75e813068eda5ff
fernsky <trilochanbhusal000@gmail.com>
2025-05-05 14:42:37 +0545

    refactor: remove unused NextSSRPlugin and uploadthing imports from layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,9 +6,7 @@ import { locales } from "@/i18n/config";
 import { GeistSans } from "geist/font/sans";
 import { validateRequest } from "@/lib/auth/validate-request";
 import { Providers } from "@/components/providers";
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
-import { uploadRouter } from "@/app/api/uploadthing/core";
 
 import "@/app/globals.css";
 
@@ -221,7 +219,6 @@ export default async function RootLayout(props: {
         <meta name="gov:confidentiality" content="public" />
       </head>
       <body className={fontClass}>
-        <NextSSRPlugin routerConfig={extractRouterConfig(uploadRouter)} />
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Providers user={user}>{children}</Providers>
         </NextIntlClientProvider>
